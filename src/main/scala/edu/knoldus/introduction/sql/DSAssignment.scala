c279e7d87ddaa43b74963bc1eac4c16a7380c14f
Himanshu Gupta <himanshu@knoldus.com>
2016-08-10 11:33:02 +0530

    corrected code structure of assignment 2

--- src/main/scala/edu/knoldus/introduction/sql/DSAssignment.scala
+++ src/main/scala/edu/knoldus/introduction/sql/DSAssignment.scala
@@ -3,43 +3,58 @@ package edu.knoldus.introduction.sql
 import java.sql.Timestamp
 import java.util.Calendar
 
-import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
 import org.apache.spark.sql.functions._
 
-object DSAssignment extends App {
+object GlobalData {
   val spark = SparkSession.builder().appName("Assignment2").master("local[*]").config("spark.sql.shuffle.partitions", 8).getOrCreate()
 
-  import spark.implicits._
+  def dataFrame(path: String): DataFrame = spark.read.option("header", true).csv(path)
+}
 
-  val fireServiceCallsDF = spark.read.option("header", true).csv("src/main/resources/Fire_Department_Calls_for_Service.csv")
+object FireDepartment {
+  import GlobalData._
 
-  val callTypes = fireServiceCallsDF.select("Call Type").distinct().count()
-  println(s"No. of different types of calls made to Fire Department = $callTypes")
+  private val callTypeColName = "Call Type"
+  private val callDateTimestampColName = "Call Date Timestamp"
 
-  val callTypeIncidents = fireServiceCallsDF.select("Call Type").groupBy("Call Type").count()
-  println("No. of incidents of each call type are as follows:")
-  callTypeIncidents.show(callTypes.toInt)
-
-  val fireServiceCallsTSDF = fireServiceCallsDF.withColumn("Call Date Timestamp", unix_timestamp(fireServiceCallsDF("Call Date"), "MM/dd/yyyy")
+  val fireServiceCallsDF = dataFrame("src/main/resources/Fire_Department_Calls_for_Service.csv")
+  val fireServiceCallsTSDF = fireServiceCallsDF.withColumn(callDateTimestampColName, unix_timestamp(fireServiceCallsDF("Call Date"), "MM/dd/yyyy")
     .cast("timestamp"))
 
-  val callsTSDF = fireServiceCallsTSDF("Call Date Timestamp")
+  val callsTSDF = fireServiceCallsTSDF(callDateTimestampColName)
 
-  val yearsOfServiceCalls = fireServiceCallsTSDF.select(year(callsTSDF)).distinct().count()
-  println(s"No. of years of Fire Service Calls in data = $yearsOfServiceCalls")
 
+  def callTypes: Long = fireServiceCallsDF.select(callTypeColName).distinct().count()
+
+  def callTypeIncidents: DataFrame = fireServiceCallsDF.select(callTypeColName).groupBy(callTypeColName).count()
+
+  def yearsOfServiceCalls: Long = fireServiceCallsTSDF.select(year(callsTSDF)).distinct().count()
+
+  // Spark SQL Encoder for java.sql.Timestamp
   implicit val timestampEncoder = Encoders.kryo[Timestamp]
 
   val currentTimeStamp = new Timestamp(Calendar.getInstance().getTime.getTime)
   val maxDateInData = fireServiceCallsTSDF.select(max(callsTSDF)).map(_.getTimestamp(0)).collect().headOption.fold(currentTimeStamp)(identity)
   val maxLocalDateTime = maxDateInData.toLocalDateTime
 
-  val lastSevenDaysCalls = fireServiceCallsTSDF.filter(year(callsTSDF) === maxLocalDateTime.getYear)
-    .filter(dayofyear(callsTSDF) >= (maxLocalDateTime.getDayOfYear - 6)).groupBy(dayofyear(callsTSDF))
-    .count().select(sum("count")).map(_.getLong(0)).collect().headOption.fold(0L)(identity)
-  println(s"No. of service calls logged in last 7 days = $lastSevenDaysCalls")
 
-  val topNeighbourhood = fireServiceCallsTSDF.filter(year(callsTSDF) === (maxLocalDateTime.getYear - 1)).groupBy("Neighborhood  District").count()
-    .orderBy(desc("count")).select("Neighborhood  District").collect().map(_.getString(0)).headOption.fold("")(identity)
+  def lastSevenDaysCalls: Long = fireServiceCallsTSDF.filter(year(callsTSDF) === maxLocalDateTime.getYear)
+    .filter(dayofyear(callsTSDF) >= (maxLocalDateTime.getDayOfYear - 6)).groupBy(dayofyear(callsTSDF))
+    .count().select(sum("count")).first().getLong(0)
+
+  def topNeighbourhood: String = fireServiceCallsTSDF.filter(year(callsTSDF) === (maxLocalDateTime.getYear - 1)).groupBy("Neighborhood  District").count()
+    .orderBy(desc("count")).select("Neighborhood  District").first().getString(0)
+}
+
+object DSAssignment extends App {
+  import FireDepartment._
+
+  println(s"No. of different types of calls made to Fire Department = $callTypes")
+  println("No. of incidents of each call type are as follows:")
+  callTypeIncidents.show(callTypes.toInt)
+
+  println(s"No. of years of Fire Service Calls in data = $yearsOfServiceCalls")
+  println(s"No. of service calls logged in last 7 days = $lastSevenDaysCalls")
   println(s"The neighbourhood in SF that generated most calls last year is: $topNeighbourhood")
 }
