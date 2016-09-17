0f5a173475c186e4ccfccadb8316cdd159f6ce3a
Himanshu Gupta <himanshu@knoldus.com>
2016-09-17 19:49:39 +0530

    created a java example of structured streaming

---
+++ src/main/java/edu/knoldus/introduction/structured/JavaStructuredNetworkWordCount.java
@@ -0,0 +1,49 @@
+package edu.knoldus.introduction.structured;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQuery;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class JavaStructuredNetworkWordCount {
+    public static void main(String args[]) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaStructuredNetworkWordCount")
+                .master("local")
+                .config("spark.sql.shuffle.partitions", 8)
+                .getOrCreate();
+
+        // Create DataFrame representing the stream of input lines from connection to localhost:9999
+        Dataset<Row> lines = spark
+                .readStream()
+                .format("socket")
+                .option("host", "localhost")
+                .option("port", 9999)
+                .load();
+
+       // Split the lines into words
+        Dataset<String> words = lines
+                .as(Encoders.STRING())
+                .flatMap(
+                        new FlatMapFunction<String, String>() {
+                            @Override
+                            public Iterator<String> call(String x) {
+                                return Arrays.asList(x.split(" ")).iterator();
+                            }
+                        }, Encoders.STRING());
+
+        // Generate running word count
+        Dataset<Row> wordCounts = words.groupBy("value").count();
+
+        // Start running the query that prints the running counts to the console
+        StreamingQuery query = wordCounts.writeStream()
+                .outputMode("complete")
+                .format("console")
+                .start();
+
+        query.awaitTermination();
+    }
+}
